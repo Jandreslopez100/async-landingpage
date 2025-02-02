51207134cec4056381ca585a894cb00db8ebbcf6
Jairo Andrés <jandreslopez100@gmail.com>
2025-02-02 10:01:26 +0100

    Creación del html e implementación parcial de la API de youtube

---
+++ src/assets/main.js
@@ -0,0 +1,39 @@
+const API = 'https://youtube-v311.p.rapidapi.com/playlists/?part=snippet&id=PLeSGp-0Zg2WrrQnMA1sT183hk5871md28&maxResults=5';
+
+const content = null || document.getElementById('content');
+
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'c3f1198844mshfc84a1ac8073ba9p1f95f4jsna845f7899e11',
+		'x-rapidapi-host': 'youtube-v311.p.rapidapi.com'
+	}
+};
+
+async function fetchData(urlApi) {
+    const response = await fetch(urlApi, options);
+    const data = await response.json();
+    return data;
+}
+
+(async () => {
+    try {
+        const videos = await fetchData(API);
+        let view = `
+        ${videos.items.map(video => `
+            <div class="group relative">
+            <div
+                class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+                <img src="${video.snippet.thumbnail.high.url}" alt="${video.snippet.description}" class="w-full">
+            </div>
+            <div class="mt-4 flex justify-between">
+                <h3 class="text-sm text-gray-700">
+                <span aria-hidden="true" class="absolute inset-0"></span>
+                ${video.snippet.title}
+                </h3>
+            </div>
+            </div>
+        `).slice(0,4).join('')}
+        `;
+    }
+})();
